ca1421da721b8e218b52bf8a929d40868ae8ff14
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-03-19 22:47:14 +0200

    Add files via upload

---
+++ Task_1/task1.cpp
@@ -0,0 +1,20 @@
+//Змінити порядок розміщення елементів в масиві на зворотний.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //work 1 
+    const int SIZE = 15;
+    int arr[SIZE];
+    for (int i = 0; i <= SIZE; i++)//standart
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (int i = SIZE; i >= 0; --i) //reverse
+    {
+        cout << i << " ";
+    }
+}
