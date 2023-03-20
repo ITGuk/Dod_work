0c66f5f70dc817d44f165c5bbc447d50136ca58f
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-03-20 11:30:59 +0200

    Update task4.cpp

--- Task_4/task4.cpp
+++ Task_4/task4.cpp
@@ -1,5 +1,5 @@
-//Перетворити масив – переставити місцями його елементи таким чином, щоб спочатку
-//розташовувалися всі елементи, рівні нулю, а потім – всі інші.
+//Convert the array - rearrange its elements in such a way that at first
+//all elements equal to zero were located, and then all others.
 
 #include <iostream>
 using namespace std;
@@ -12,24 +12,24 @@ int main()
     srand(time(NULL));
     for ( int i = 0; i < SIZE; i++)
     {
-        double a = rand() % 30;//рандомні числа від 0 до 30
-        double b = 0.10 * a;//переводим число в дробне шоб була цифра після крапки
+        double a = rand() % 30;//random numbers from 0 to 30
+        double b = 0.10 * a;//convert the number to a fraction so that there is a digit after the period
         arr[i] = b;
-        cout << arr[i] << " ";//вивід рандомних чисел
+        cout << arr[i] << " ";
     }
     cout << endl;
     for (int i = 0; i < SIZE; i++)
     {
         if (arr[i]  < 1)
         {
-            cout << arr[i] << " ";//вивід першої половини масива(меншої)
+            cout << arr[i] << " ";//output of the first (smaller) half of the array
         }
     }
     for (int i = 0; i < SIZE; i++)
     {
         if (arr[i] >= 1)
         {
-            cout << arr[i] << " ";//вивід другої половини масива (більшої)
+            cout << arr[i] << " ";//output of the second (bigger) half of the array
         }
     }
 }
