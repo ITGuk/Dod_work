247c28ca533bf6adf62586822e9dcb7d2a72fafc
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-03-20 11:37:38 +0200

    Update task5.cpp

--- Task_5/task5.cpp
+++ Task_5/task5.cpp
@@ -1,6 +1,6 @@
-//ѕеретворити масив Ц переставити м≥сц€ми його елементи так, щоб в перш≥й його половин≥
-//були розм≥щен≥ елементи, що сто€ли в непарних позиц≥€х, а в друг≥й половин≥ Ц елементи,
-//що сто€ли в парних позиц≥€х.
+//Convert the array - rearrange its elements so that in the first half of it
+//elements standing in odd positions were placed, and in the second half - elements,
+//that stood in even positions.
 
 #include <iostream>
 using namespace std;
@@ -8,52 +8,52 @@ using namespace std;
 int main()
 {
     const int SIZE = 15;
-    int arr[SIZE] = {};//массив з €кого беремо парн≥ ≥ не парн≥ числа
-    int arr_nepar[SIZE] = {};//тимчасовий массив дл€ непарних чисел
-    int arr_par[SIZE] = {};//тимчасовий массив дл€ парних чисел
-    int zagal[SIZE] = {};//массив в €кому вже в≥дсортован≥ числа 
+    int arr[SIZE] = {};//array from which we take even and odd numbers
+    int arr_nepar[SIZE] = {};//temporary array for odd numbers
+    int arr_par[SIZE] = {};//temporary array for even numbers
+    int zagal[SIZE] = {};//array in which the numbers are already sorted
     if (SIZE % 2 != 0)
     {
         for (int i = 0; i < SIZE; i++)
         {
-            arr[i] = i;//наповненн€ массиву
+            arr[i] = i;
         }
 
         for (int i = 0; i < SIZE; i++)
         {
-            cout << arr[i] << " ";//вив≥д нев≥дсортованого масиву
+            cout << arr[i] << " ";//output of an unsorted array
         }
         cout << endl;
-        int j = 0;//л≥чильник дл€ проходженн€ циклу пошуку непарних чисел
+        int j = 0;//counter for going through the cycle of searching for odd numbers
         for (int i = 0; i < SIZE; i++)
         {
             if (arr[i] % 2 != 0)
             {
-                arr_nepar[j] = i;//наповненн€ тимчасового масива непарними елементами
+                arr_nepar[j] = i;//filling the temporary array with odd elements
                 j++;
             }
         }
-        int j1 = 0;//л≥чильник дл€ проходженн€ циклу пошуку парних чисел
+        int j1 = 0;//counter for going through the cycle of searching for even numbers
         for (int i = 0; i < SIZE; i++)
         {
             if (arr[i] % 2 == 0)
             {
-                arr_par[j1] = i;//наповненн€ тимчасового масива парними елементами
+                arr_par[j1] = i;//filling the temporary array with even elements
                 j1++;
             }
         }
         int k = 0;
         for (int i = 0; i < SIZE / 2; i++)
         {
-            zagal[k++] = arr_nepar[i];//наповнюЇм початок масиву непарними елементами
+            zagal[k++] = arr_nepar[i];//fill the beginning of the array with odd elements
         }
         for (int i = 0; i <= SIZE / 2; i++)
         {
-            zagal[k++] = arr_par[i];//наповнюЇм другу половину масиву парними елементами
+            zagal[k++] = arr_par[i];//fill the second half of the array with even elements
         }
         for (int i = 0; i < SIZE; i++)
         {
-            cout << zagal[i] << " ";//вив≥д в≥дсортованого масиву
+            cout << zagal[i] << " ";
         }
     }
 }
